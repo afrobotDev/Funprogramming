c989457da193c62d16c3698fd320c5ac8ca265dd
YitbarekE <yitbarekendale.2024@gmail.com>
2026-04-07 16:47:03 +0300

    Remove mutation and side effects from pure_fuc

---
+++ chapter_three/pure_func.py
@@ -0,0 +1,24 @@
+default_commands = {}
+default_formats = ["txt", "md", "html"]
+saved_documents = {}
+
+def add_custom_command(commands, new_command, function):
+    commands_cpy = commands.copy() 
+    commands_cpy[new_command] = function
+    return commands_cpy 
+
+
+def add_format(formats, format):
+    formats_cpy = formats.copy()
+    formats_cpy.append(format)
+    return formats_cpy 
+
+
+def save_document(docs, file_name, doc):
+    docs_cpy = docs.copy()
+    docs_cpy[file_name] = doc
+    return docs_cpy 
+
+
+def add_line_break(line):
+    return line + "\n\n"
