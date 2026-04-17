095ff6999110432e6912fe4af87d9b302e5a74c4
YitbarekE <yitbarekendale.2024@gmail.com>
2026-04-17 21:49:32 +0300

    fix: for loop removed

--- chapter_six/updated_collection.py
+++ chapter_six/updated_collection.py
@@ -1,9 +1,8 @@
 def new_collection(initial_docs):
-    copy_initial_docs = initial_docs.copy()
-    for item in initial_docs:
-        def add_doc(item):
-            copy_initial_docs.append(item)
-            return copy_initial_docs
-    
-        return add_doc
+    initial_docs_cpy = initial_docs.copy()
+    def add_doc(doc):
+        initial_docs_cpy.append(doc)
+        
+        return initial_docs_cpy
 
+    return add_doc
