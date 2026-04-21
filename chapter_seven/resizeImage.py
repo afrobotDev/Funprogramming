c0a9815e7f6f9cdb0017d628a9887e45689cf473
YitbarekE <yitbarekendale.2024@gmail.com>
2026-04-21 18:11:36 +0300

    Add resizeImage.py and its test

---
+++ chapter_seven/resizeImage.py
@@ -0,0 +1,13 @@
+def new_resizer(max_width, max_height):
+    def inner_resizer(min_width=0, min_height=0):
+        if min_width > max_width or min_height > max_height:
+            raise Exception("minimum size cannot exceed maximum size")
+
+        def inner_dimensions(width, height):
+            width = max(min_width, min(width, max_width))
+            height = max(min_height, min(height, max_height))
+            return width, max_height
+
+        return inner_dimensions
+    return inner_resizer
+
