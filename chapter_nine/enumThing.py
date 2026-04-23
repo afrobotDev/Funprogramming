8008afcb865ef580bf7133473a5c3506e45d8c08
YitbarekE <yitbarekendale.2024@gmail.com>
2026-04-23 12:02:43 +0300

    Add enumThing.py

---
+++ chapter_nine/enumThing.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+class Doctype(Enum):
+    PDF = ''
+    TXT = ''
+    DOCX = ''
+    MD = ''
+    HTML = ''
+
+
+#Doctype = Enum('DocType', ['PDF','TXT','DOCX','MD','HTML'])
