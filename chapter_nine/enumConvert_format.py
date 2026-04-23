dbe53e57527b90c98c6f6531896e37d3284cfc5b
YitbarekE <yitbarekendale.2024@gmail.com>
2026-04-23 15:43:37 +0300

    Add enumConvert_fomrat.py

---
+++ chapter_nine/enumConvert_format.py
@@ -0,0 +1,20 @@
+from enum import Enum
+
+DocFormat = Enum("DocFormat", ["PDF", "TXT", "MD", "HTML"])
+
+
+def convert_format(content, from_format, to_format):
+    match (from_format, to_format):
+        case (DocFormat.MD, DocFormat.HTML):
+            return content.replace('# ', '<h1>') + '</h1>'
+
+        case (DocFormat.TXT, DocFormat.PDF):
+            return f'[PDF] {content} [PDF]'
+
+        case (DocFormat.HTML, DocFormat.MD):
+                return content.replace('<h1>', '# ').replace('</h1>','')
+            
+        case _:
+            raise ValueError('invalid type')
+
+
